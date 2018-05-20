e536eea6c0327bc3b395e413b7ce179473f00a22
akhesaCaro <caroline.gaudreau@gmail.com>
2018-05-20 10:50:41 +0200

    End result of first coding session.

--- src/SpaceInvaders.hs
+++ src/SpaceInvaders.hs
@@ -16,8 +16,11 @@ import Window
 -- *********************** Game state ****************************
 
 -- | Game type
-newtype Game = Game
+data Game = Game
   { library :: ImageLibrary
+  , spaceship :: Position
+  , monsters :: [Position]
+  , mDirection :: Direction
   }
 
 -- | Image library
@@ -27,17 +30,27 @@ data ImageLibrary = ImageLibrary
   , monster1Img :: Gloss.Picture
   }
 
--- | spaceship
-newtype Spaceship = Spaceship
-  { pos :: (Float, Float)
-  }
+-- | position
+type Position = (Float, Float)
+
+data Direction = Up | Down
 
 -- | Create the initial game state from an image library.
+-- generateur de -300 à 300 en x et -300 à 0 en y de 100 en 100
+generateMonstersPosition
+  :: [Position]
+generateMonstersPosition =
+  [(i*100,j*100) | i <- [-3..3],
+         j <- [0..3] ]
+
 mkInitialState
   :: ImageLibrary -- ^ Image library
   -> Game    -- ^ Initial game state
 mkInitialState l = Game
   { library = l -- Set the game image library as the argument.
+  , spaceship = (0, -250)
+  , monsters = generateMonstersPosition
+  , mDirection = Down
   }
 
 -- *********************** Updating game ************************
@@ -48,23 +61,62 @@ update
   -> Game -- ^ Current game state
   -> Game -- ^ Updated game state.
 -- Game playing
-update seconds game = moveSpaceship game
+update seconds game =
+  moveMonsters game
+
+move
+  :: Float
+  -> Float
+  -> Position
+  -> Position
+move dx dy (x, y) =
+  (x + dx, y + dy)
 
 -- | Move spaceship.
 moveSpaceship
-  :: Game -- ^ Game state to update
+  :: Float
+  -> Game -- ^ Game state to update
   -> Game -- ^ Game updated
-moveSpaceship game = game
+moveSpaceship delta game = game {
+    spaceship = move delta 0 (spaceship game)
+  }
+
+moveMonsters
+  :: Game
+  -> Game
+moveMonsters game =
+  game {
+    mDirection = if getLowestPosition (monsters game) < -200 then Up else (mDirection game)
+    , monsters = fmap (move 0 (directionFactor (mDirection game))) (monsters game)
+  }
+
+directionFactor
+  :: Direction
+  -> Float
+directionFactor direction = case direction of
+  Up -> 1
+  Down -> -1
+
+getLowestPosition
+  :: [Position]
+  -> Float
+getLowestPosition = foldr minPos 3000
+  where minPos (_, y) minValue = min y minValue
+
 -- Hint: use record update syntax.
 
 -- *********************** Key handling ************************
+
 
 -- | Modify 'Game' state based on key events.
 handleKeys
   :: Gloss.Event -- ^ keyEvent
   -> Game -- ^ current game state
   -> Game -- ^ Game updated
+handleKeys (Gloss.EventKey (Gloss.SpecialKey Gloss.KeyLeft) Gloss.Down _ _) game = moveSpaceship (-10) game
+handleKeys (Gloss.EventKey (Gloss.SpecialKey Gloss.KeyRight) Gloss.Down _ _) game = moveSpaceship 10 game
 handleKeys _ game = game
+
 -- Hint: pattern-match on event key parameter (see Gloss documentation).
 
 -- *********************** Rendering *****************************
@@ -75,8 +127,8 @@ renderGame
   -> Gloss.Picture -- ^ A picture of this game state
 renderGame game = Gloss.pictures
   [ renderBackground (library game)
-  , renderSpaceship (library game) (0, -250)
-  , renderMonster (library game) (0, 250)
+  , renderSpaceship (library game) (spaceship game)
+  , renderMonsters (library game) (monsters game)
   ]
 
 -- | Render the background image.
@@ -110,9 +162,9 @@ renderMonster library (x, y) =
 -- | Render multiple monsters in one go.
 renderMonsters
   :: ImageLibrary
-  -> [(Float, Float)] -- ^ Monster positions.
+  -> [Position] -- ^ Monster positions.
   -> Gloss.Picture -- ^ Collage picture with all monsters represented.
-renderMonsters = undefined
+renderMonsters imageLib monsters = Gloss.pictures (fmap (renderMonster imageLib) monsters)
 -- Hint: think fmap, think currying !
 
 -- | Render score
