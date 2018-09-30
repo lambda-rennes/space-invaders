71f8a85eb88591cb255a8e054dd7263710e71fcf
Gaël Deest <gael.deest@tweag.io>
2018-09-30 21:01:48 +0200

    getNewMissiles

--- src/Connector.hs
+++ src/Connector.hs
@@ -22,39 +22,50 @@ type XPosition = Float
 
 data Connector = Connector
   { sendMessage :: Message -> IO ()
+  , getNewMissiles :: IO [(PlayerID, XPosition)]
   }
 
 startConnector
   :: PlayerID -- ^ PlayerID
+  -- ^ Player position map
   -> MVar (Map PlayerID XPosition)
   -> IO Connector
 startConnector playerId playersMap = do
   tcp <- tcpClient "localhost" 7777
-  chan <- atomically newTChan
+  msgChan <- atomically newTChan
+  missilesChan <- atomically newTChan
   let run = runSession tcp
-  _ <- forkIO . run $ receiveThread playersMap
-  _ <- forkIO . run $ sendThread playerId chan
-  return $ Connector  $
-    atomically . writeTChan chan
+  _ <- forkIO . run $ receiveThread playersMap missilesChan
+  _ <- forkIO . run $ sendThread playerId msgChan
+  return $ Connector {
+    sendMessage = atomically . writeTChan msgChan
+  , getNewMissiles = atomically $ readAll missilesChan
+  }
+
+readAll
+  :: TChan a
+  -> STM [a]
+readAll = readAll' []
+  where
+    readAll' xs chan = do
+      mbX <- tryReadTChan chan
+      case mbX of
+        Nothing -> return xs
+        Just x -> readAll' (x:xs) chan
 
 receiveThread
   :: MVar (Map PlayerID XPosition)
+  -> TChan (PlayerID, XPosition)
   -> TransportM ()
-receiveThread playersMap = recvtForever $ \t ->
+receiveThread playersMap missilesChan = recvtForever $ \t ->
   case readBERT t of
     Left err -> liftIO $ putStrLn err
     Right (player, msg) ->
       case msg of
         NewPosition pos -> liftIO $ modifyMVar_ playersMap $
-          return . Map.insert player (read pos)
-  -- case t of
-  --   BinaryTerm t' ->
-  --     case readBERT (decode t') of
-  --       Left err -> liftIO $ putStrLn err
-  --       Right (player, msg) ->
-  --         case msg of
-  --           NewPosition pos -> liftIO $ modifyMVar_ playersMap $
-  --             return . Map.insert player (read pos)
+          return . Map.insert player pos
+        Missile pos -> liftIO $ atomically $
+          writeTChan missilesChan (player, pos)
 
 sendThread
   :: PlayerID
